de0a6cf30f32fb3871f444b7006efb889a246f02
auliakhanzant <auliakhanzant@gmail.com>
2026-01-30 19:04:45 +0700

    Add files via upload

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if (!isset($_SESSION['user_status']) || $_SESSION['user_status'] != 2) {
+if (!isset($_SESSION['user_status']) || $_SESSION['user_status'] != 1) {
     echo "<script>alert('Anda tidak memiliki akses!'); window.location.href='../login.php';</script>";
     exit;
 }
@@ -10,20 +10,21 @@ if (!isset($_SESSION['user_status']) || $_SESSION['user_status'] != 2) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kasir - Penjualan Khanza</title>
+    <title>Admin - Penjualan Khanza</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-info">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand text-white" href="index.php">Kasir Panel</a>
+            <a class="navbar-brand" href="index.php">Admin Panel</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
-                    <li class="nav-item"><a class="nav-link text-white" href="index.php">Dashboard</a></li>
-                    <li class="nav-item"><a class="nav-link text-white" href="transaksi.php">Transaksi Penjualan</a></li>
+                    <li class="nav-item"><a class="nav-link" href="index.php">Dashboard</a></li>
+                    <li class="nav-item"><a class="nav-link" href="user.php">Manajemen User</a></li>
+                    <li class="nav-item"><a class="nav-link" href="barang.php">Manajemen Barang</a></li>
                     <li class="nav-item"><a class="nav-link btn btn-danger btn-sm text-white ms-2" href="../logout.php">Logout</a></li>
                 </ul>
             </div>
